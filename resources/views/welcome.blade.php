5ad8519abdf299021ef703f720be7021d50b7dd9
Unni <69468073+hash-dotcom@users.noreply.github.com>
2020-11-21 20:02:13 +0530

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,9 @@
 <html>
     <head>
     </head>
-    <style>
-body {
-  background-image: url('D:/My Projects/Medify/medify/resources/medify_background.jpg');
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: 100% 100%;
-}
-</style>
-    <body>
+    <body >
+        <img src="resources\css\medify_background.jpg" height="500px" width="500px">
+        </img>
+    
     </body>
 </html>
