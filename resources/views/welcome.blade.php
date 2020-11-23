d658bd308087c344b31d651945df0794dcd52c3a
Unni <69468073+hash-dotcom@users.noreply.github.com>
2020-11-23 19:03:42 +0530

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <link rel="stylesheet" href="url('css/style.css')"/>
+       <link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
     </head>
+
+    <!-- ////////////////////////////////////////////////////////////// -->
+
+
     <body >
+
     <div>
        <h1>
        Medify
        </h1>
     </div>
+    
     </body>
 </html>
