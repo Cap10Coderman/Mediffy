fedddc26357e172569797902e6da6f1d03a51092
Unni <69468073+hash-dotcom@users.noreply.github.com>
2020-11-23 22:07:36 +0530

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,11 +10,27 @@
     <body >
 
     
-       <br> 
-       <h1>
-           
-        &nbsp;  Mediffy 
-       </h1>
+      <div class="split left">
+         <div class="centered">
+                   <h1>
+                       Mediffy 
+                  </h1>
+          </div>
+      </div>     
+
+      <div class="split right">
+         <div class="centered">
+                     
+            <button class="button button1" style="vertical-align:middle"><span>DOCTOR</span></button>
+            <BR>
+            <button class="button button2" style="vertical-align:middle"><span>PATIENT</span></button>
+            <BR>
+            <button class="button button3" style="vertical-align:middle"><span>PHARMACY<span></button>
+
+                     
+         </div>
+      </div>
+                     
         
     
 
