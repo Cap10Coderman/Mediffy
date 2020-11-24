0e0068fd47de6841f6cdc88cb79237653700c25c
Unni <69468073+hash-dotcom@users.noreply.github.com>
2020-11-24 18:25:18 +0530

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,8 @@
 <html>
     <head>
        <link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
+       
+
     </head>
 
     <!-- ////////////////////////////////////////////////////////////// -->
@@ -11,24 +13,45 @@
 
     
       <div class="split left">
-         <div class="centered">
+         <div class="centered1">
                    <h1>
                        Mediffy 
                   </h1>
+                  
+                <button class="button button1" style="vertical-align:middle"><span>DOCTOR LOGIN</span></button>
+                  
+                  <button class="button button2" style="vertical-align:middle"><span>PATIENT LOGIN</span></button>
+               
+                  <button class="button button3" style="vertical-align:middle"><span>PHARMACY LOGIN</span></button>
+                               
           </div>
       </div>     
 
       <div class="split right">
-         <div class="centered">
+         <div class="centered2">
                      
-            <button class="button button1" style="vertical-align:middle"><span>DOCTOR</span></button>
-            <BR>
-            <button class="button button2" style="vertical-align:middle"><span>PATIENT</span></button>
-            <BR>
-            <button class="button button3" style="vertical-align:middle"><span>PHARMACY<span></button>
-
+            <div class="login-box">
+               <h2> Doctor Login</h2>
+               <form action="">
+                  <div class="user-box">
+                     <input type="text" name="" required="">
+                     <label>DocId</label>
+                  </div>
+                  <div class="user-box">
+                     <input type="password">
+                     <label for="">Password</label>
+                  </div>
+                  <a href="">
+                     <span></span>
+                     <span></span>
+                     <span></span>
+                     <span></span>
+                     LOGIN
+                  </a>
+               </form>
+            </div>
                      
-         </div>
+          </div>
       </div>
                      
         
