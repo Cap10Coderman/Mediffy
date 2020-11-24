4aabfb1cef814c488715570692b674c7c5ac924c
Unni <69468073+hash-dotcom@users.noreply.github.com>
2020-11-24 18:54:28 +0530

    init

---
+++ public/js/visibility.js
@@ -0,0 +1,11 @@
+
+function docformvisibility() {
+         document.getElementById("doc").style.visibility = "visible";
+         document.getElementById("pat").style.visibility = "hidden";
+       }
+   
+       function patformvisibility() {
+        document.getElementById("doc").style.visibility = "hidden";  
+        document.getElementById("pat").style.visibility = "visible";
+      }
+  
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 <html>
     <head>
        <link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
-       
 
+       <script src="{{ url('js/visibility.js') }}"></script>
     </head>
 
     <!-- ////////////////////////////////////////////////////////////// -->
@@ -18,9 +18,9 @@
                        Mediffy 
                   </h1>
                   
-                <button class="button button1" style="vertical-align:middle"><span>DOCTOR LOGIN</span></button>
+                <button class="button button1" style="vertical-align:middle" onclick="docformvisibility()"><span>DOCTOR LOGIN</span></button>
                   
-                  <button class="button button2" style="vertical-align:middle"><span>PATIENT LOGIN</span></button>
+                  <button class="button button2" style="vertical-align:middle" onclick="patformvisibility()"><span>PATIENT LOGIN</span></button>
                
                   <button class="button button3" style="vertical-align:middle"><span>PHARMACY LOGIN</span></button>
                                
@@ -30,16 +30,39 @@
       <div class="split right">
          <div class="centered2">
                      
-            <div class="login-box">
+            <div class="login-box" id="doc">
                <h2> Doctor Login</h2>
-               <form action="">
+               <form action="" id="docform">
                   <div class="user-box">
                      <input type="text" name="" required="">
-                     <label>DocId</label>
+                     <label>Doctor Id</label>
                   </div>
                   <div class="user-box">
-                     <input type="password">
-                     <label for="">Password</label>
+                     <input type="password" name="" required="">
+                     <label>Password</label>
+                  </div>
+                  <a href="">
+                     <span></span>
+                     <span></span>
+                     <span></span>
+                     <span></span>
+                     LOGIN
+                  </a>
+               </form>
+            </div>
+
+            <!--PATINT FORM--->
+
+            <div class="login-box" id="pat">
+               <h2> Patient Login</h2>
+               <form action="" id="patform">
+                  <div class="user-box">
+                     <input type="text" name="" required="">
+                     <label>Patient Id</label>
+                  </div>
+                  <div class="user-box">
+                     <input type="password" name="" required="">
+                     <label>Password</label>
                   </div>
                   <a href="">
                      <span></span>
